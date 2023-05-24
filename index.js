e32318bb1e962e3715e1431fc0c63defdba25eaf
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-23 23:49:35 -0500

    updated env sample

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 'use strict';
 
-// Start up DB Server
-const { db } = require('./src/auth/models/index.js');
-db.sync()
-  .then(() => {
+require('dotenv').config();
+const PORT = process.env.PORT || 3003;
+// const { sequelize } = require('./src/auth/models');
+// const { start } = require('./src/server');
 
-    // Start the web server
-    require('./src/server.js').start(process.env.PORT);
-  });
+// sequelize.sync()
+//   .then(() => {
+//     console.log('Successful DB connection');
+//     start(PORT);
+//   })
+//   .catch((err) => {
+//     console.error('could not start server', err);
+//   });
+
+
+
+
+// // Start up DB Server
+const { db } = require('./src/auth/models/index.js');
+// db.sync()
+//   .then(() => {
+
+//     // Start the web server
+//     require('./src/server.js').start(process.env.PORT);
+//   });
