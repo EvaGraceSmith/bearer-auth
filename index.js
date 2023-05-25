4fba7f239de78fd6125751016fca0f5aaf2d272f
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-25 11:03:38 -0500

    added expiration

--- index.js
+++ index.js
@@ -2,22 +2,10 @@
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3003;
-// const { sequelize } = require('./src/auth/models');
-// const { start } = require('./src/server');
-
-// sequelize.sync()
-//   .then(() => {
-//     console.log('Successful DB connection');
-//     start(PORT);
-//   })
-//   .catch((err) => {
-//     console.error('could not start server', err);
-//   });
 
 
 
-
-// // Start up DB Server
+// Start up DB Server
 const { db } = require('./src/auth/models/index.js');
 db.sync()
   .then(() => {
--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -6,10 +6,10 @@ const { user } = require('../models/index.js');
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return  'no _authError();'; }
-console.log('req.headers.authorization', req.headers.authorization);
+
   let basic = req.headers.authorization.split(' ').pop();
   let [username, pass] = base64.decode(basic).split(':');
-console.log('username, pass', username, pass);
+
   try {
     req.user = await user.authenticateBasic(username, pass);
     next();
